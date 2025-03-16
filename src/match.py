874f725c842d22c9552c821182dd835511876242
Theo <theodorechen03@gmail.com>
2025-03-16 16:37:07 -0400

    Spring 2025

--- src/match.py
+++ src/match.py
@@ -34,8 +34,66 @@ def extract_data(filename, lorb):
     return member_obj
 
 
+<<<<<<< Updated upstream
+=======
+# Extract data for littles
+littles = extract_data('../data/Spring2025/littles.csv', 'l')[1:]
+littlesAmt = len(littles)
+# Reformat names of all little objects to be lowercase first name
+for i in littles:
+    i.name = i.name.split(" ")[0].lower()
+>>>>>>> Stashed changes
 
     
 
+<<<<<<< Updated upstream
             
 extract_data('../misc/little.csv', 'l')
+=======
+
+# Extract data for bigs
+bigs = extract_data('../data/Spring2025/bigs.csv','b')[1:]
+bigsAmt = len(bigs)
+# Reformat names of all big objects to be lowercase first name 
+for i in bigs:
+    i.name = i.name.split(" ")[0].lower()
+
+# Prints out the names of all bigs with proper formatting for big_little_matching.cpp
+print("\n{", end="")
+for i in bigs[:bigsAmt-1]:
+    print("\""+i.name+"\"", end=",")
+print("\""+bigs[bigsAmt-1].name+"\"}")
+
+
+print("\n{", end="")
+for i in bigs[:bigsAmt-1]:
+    print(i.max_num, end=",")
+print(bigs[bigsAmt-1].max_num+"}")
+
+
+for i in bigs:
+    newLittlesList = []
+    for j in i.littles:
+        newLittlesList.append(j.split(" ")[0].lower())
+    i.littles = newLittlesList
+
+for i in littles:
+    newBigsList = []
+    for j in i.bigs:
+        newBigsList.append(j.split(" ")[0].lower())
+    i.bigs = newBigsList
+
+for i in bigs:
+    for j in littles:
+        if (j.name in i.littles and i.name in j.bigs):
+            bigsRank = (i.littles.index(j.name)) 
+            littlesRank = (j.bigs.index(i.name))
+            bigsRank = (7 - bigsRank)
+            littlesRank = (7 - littlesRank)
+            print(bigsRank*littlesRank)
+        else:
+            print("0")
+
+print("Littles:",littlesAmt)
+print("Bigs:",bigsAmt)
+>>>>>>> Stashed changes
